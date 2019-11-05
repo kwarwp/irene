f90eb6891f82bc53619cceae73ab2d928e5a68b4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-05 12:38:23 -0300

    Created grace/__log__.py @2019-11-05 15:38

---
+++ grace/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Nov 05 2019 12:38:21.617 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  __version__ "19.11.05"
+               ^
+SyntaxError: invalid syntax
+'''},
