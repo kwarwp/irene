cc914ceb352abcc8f0466229315392449189283d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-05 15:19:51 -0300

    Saved grace/main.py @2019-11-05 18:19

--- grace/main.py
+++ grace/main.py
@@ -7,6 +7,7 @@ from _spy.vitollino.main import STYLE, Cena, Elemento, Texto
 STYLE.update(width=850, height="650px")
 FIOFRONT = "https://i.imgur.com/wdwjGCt.jpg"
 AJUDA = "https://i.imgur.com/510Q3z4.png"
+FREDERICK = "https://i.imgur.com/377duSy.jpg"
 
 class FioCruz:
     """ Cenário da FioCruz. """
@@ -21,13 +22,13 @@ class FioCruz:
         
     def ajudar(self, *_):
         Texto(self.fc, "Siga pela direita para entrar no lab").vai()
-        self.fc.direita = Texto(self.fc, "ok você vai chegar lá")
+        self.fc.direita = LembrarHerdeitariedade() #Texto(self.fc, "ok você vai chegar lá")
         
         
     def inicia(self):
         self.fc.vai()
 
-class LembrarHerdeitariedade:
+class EntenderHerdeitariedade:
     """Meu caro e querido aprendiz Damon, as informações que você precisa sobre HEREDITARIEDADE
     estão em um pen drive que deixei na gaveta da minha mesa que tem o fundo falso, abra essa geveta
     e pegue esse pen drive, mas lembre-se que tem pessoas interessadas em roubar a fórmula, por isso
@@ -37,7 +38,7 @@ class LembrarHerdeitariedade:
     Imagens: cena lab, cofre, pendrive, tabela
     """    
 
-class EntenderHerdeitariedade:
+class LembrarHerdeitariedade:
     """Após a morte do cientista Frederick, planejavam roubar sua fórmula em seu laboratório,
     seu fiel amigo e aprendiz Damon, precisa procurar a fórmula para protegê-la que está no laboratório,
     porém seu amigo antes de morrer disse-lhe "botei a fórmula dentro deste cofre, cujo a senha é formada
@@ -45,7 +46,16 @@ class EntenderHerdeitariedade:
     decifrar quais letras formam a palavra-chave (4-4, 5-1, 3-2, 5-1, 2-2= GENES). 
     
     Imagens: cena lab, caneca, tubo de ensaio, relógio, quadro, microscópio
-    """    
+    """
+    def __init__(self):
+        self.fc = Cena(FREDERICK, direita=self, meio=self, esquerda=self)
+        self.ajuda = Elemento(AJUDA, x=470, y=510, w=300, h=40, cena=self.fc)
+        self.ajuda.elt.style.opacity = 0
+        
+    def vai(self, *_):
+        self.fc.vai
+        Texto(self.fc, "Você chegou ao lab Frederick, procure pistas da fórmula").vai()
+
 def grace():
     fc = FioCruz()
     fc.inicia()
