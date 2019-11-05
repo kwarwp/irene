408999ad569655ce7416703567e7900ea7bad8c7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-05 14:16:34 -0300

    Saved grace/__log__.py @2019-11-05 17:16

--- grace/__log__.py
+++ grace/__log__.py
@@ -85,3 +85,27 @@ AttributeError: 'module' object has no attribute 'update'
     STYLE.update(width=850, height=650)
 AttributeError: 'module' object has no attribute 'update'
 '''},
+{'date': 'Tue Nov 05 2019 14:16:33.130 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 28
+    grace()
+  module <module> line 23
+    fc = FioCruz()
+  module <module> line 13
+    self.fc = Cena(FIOFRONTdireita=self)
+  module _spy.vitollino.main line 975
+    Cena.c(**kwargs)
+  module _spy.vitollino.main line 1018
+    imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
+AttributeError: 'FioCruz' object has no attribute '__getitem__'
+'''},
