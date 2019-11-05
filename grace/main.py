8e07b312f89c6543753ba24c25b25fec5656177c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-05 15:50:49 -0300

    Saved grace/main.py @2019-11-05 18:50

--- grace/main.py
+++ grace/main.py
@@ -1,13 +1,18 @@
 # irene.grace.main.py
 """Planejamento do jogo da Genética JAIE19
+
+v 19.11.05b - adiciona lembra hereditariedade
 """
 __author__ = "Carlo E T Oliveira"
-__version__ = "19.11.05"
+__version__ = "19.11.05b"
 from _spy.vitollino.main import STYLE, Cena, Elemento, Texto
+from collections import namedtuple
+Pista = namedtuple("Pista", "x y dica")
 STYLE.update(width=850, height="650px")
 FIOFRONT = "https://i.imgur.com/wdwjGCt.jpg"
 AJUDA = "https://i.imgur.com/510Q3z4.png"
-FREDERICK = "https://i.imgur.com/377duSy.jpg"
+FREDERICK = "https://i.imgur.com/4EtsjiX.jpg" #"https://i.imgur.com/377duSy.jpg"
+FOCO = "https://i.imgur.com/6e096Va.png"
 
 class FioCruz:
     """ Cenário da FioCruz. """
@@ -48,16 +53,33 @@ class LembrarHerdeitariedade:
     Imagens: cena lab, caneca, tubo de ensaio, relógio, quadro, microscópio
     """
     def __init__(self):
+        self.codigo = [
+        Pista(100, 70, "pista 5-5"),
+        Pista(700, 150, "pista 4-4"),
+        Pista(700, 150, "pista 5-5"),
+        Pista(700, 150, "pista 5-5"),
+        ]
+        p = self.codigo[0]
+        self.dica = 0
         self.fc = Cena(FREDERICK, direita=self, meio=self, esquerda=self)
-        self.ajuda = Elemento(AJUDA, x=470, y=510, w=300, h=40, cena=self.fc)
-        self.ajuda.elt.style.opacity = 0
+        self.ajuda = Elemento(FOCO, x=p.x, y=p.y, w=150, h=150, cena=self.fc,vai=self.pista)
+        self.ajuda.elt.style.opacity = 0.5
+        self.inicia = self.vai
+        
+    def pista(self, *_):
+        self.dica += 1
+        p = self.codigo[self.dica]
+        Texto(self.fc, p.dica).vai()
+        self.ajuda.elt.style.left = p.x
+        self.ajuda.elt.style.top = p.y
         
     def vai(self, *_):
-        self.fc.vai
+        self.fc.vai()
         Texto(self.fc, "Você chegou ao lab Frederick, procure pistas da fórmula").vai()
 
 def grace():
     fc = FioCruz()
+    fc = LembrarHerdeitariedade()
     fc.inicia()
     
     
