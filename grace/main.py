d57c23ecdb0c2ce61ab9f4ecafe94b89f9f5dd02
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-05 12:38:17 -0300

    Saved grace/main.py @2019-11-05 15:38

--- grace/main.py
+++ grace/main.py
@@ -1,1 +1,25 @@
-# irene.grace.main.py test
+# irene.grace.main.py
+"""Planejamento do jogo da Genética JAIE19
+"""
+__author__ = "Carlo E T Oliveira"
+__version__ "19.11.05"
+from _spy.vitollino.amin import STYLE, Cena, Elemento, Texto
+STYLE.update(width="1850px", height="650px")
+FIOFRONT = "https://i.imgur.com/wdwjGCt.jpg"
+
+class FioCruz:
+    """ Cenário da FioCruz. """
+    def __init__(self):
+        self.fc = Cena(FIOFRONT)
+        
+    def vai(self):
+        self.fc.vai()
+        
+        
+def grace():
+    fc = FioCruz()
+    fc.vai()
+    
+    
+if __name__ == "__main__":
+    grace()
