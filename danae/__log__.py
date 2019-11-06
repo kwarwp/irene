3cd401d787c8c99f4b5e980cad5d626adbbdd863
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-06 08:47:08 -0300

    Created danae/__log__.py @2019-11-06 11:47

---
+++ danae/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Nov 06 2019 08:47:06.286 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 59
+  self.decide.update(s:self.entra)
+                       ^
+SyntaxError: invalid syntax
+'''},
