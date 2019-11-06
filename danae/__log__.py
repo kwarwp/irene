37d8979003815c65777c58718c6e26de77e93d8a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-06 08:47:28 -0300

    Saved danae/__log__.py @2019-11-06 11:47

--- danae/__log__.py
+++ danae/__log__.py
@@ -6,3 +6,10 @@
                        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Nov 06 2019 08:47:27.580 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 67
+  input("Uma sábia decisão, seria loucura explorar este templo macabro!)]
+                                                                         ^
+SyntaxError: EOL while scanning string literal
+'''},
