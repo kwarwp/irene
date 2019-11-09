a67f256e0b350e636e265b534c8f7fd7be0cb957
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 13:11:01 -0300

    Saved grace/__log__.py @2019-11-09 16:11

--- grace/__log__.py
+++ grace/__log__.py
@@ -177,3 +177,23 @@ SyntaxError: invalid syntax
                                                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Nov 09 2019 13:11:00.602 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 262
+    grace()
+  module <module> line 256
+    fc = LembrarHerdeitariedade()
+  module <module> line 224
+    self.ajuda.elt.style.opacity = 0.01
+AttributeError: 'LembrarHerdeitariedade' object has no attribute 'ajuda'
+'''},
