2c7c6d46adfb4257f21668a09826cb275ab022cb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-10 07:58:18 -0300

    Saved roxanne/main.py @2019-11-10 10:58

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,39 +1,26 @@
 # irene.roxanne.main.py
-# irene.grace.main.py
 """Planejamento do jogo da Genética JAIE19
-
-v 19.11.05b - adiciona lembra hereditariedade
 """
-__author__ = "Carlo E T Oliveira"
-__version__ = "19.11.05b"
+__author__ = "<O seu nome aqui>"
+__version__ = "19.11.11"
 from _spy.vitollino.main import STYLE, Cena, Elemento, Texto
-from collections import namedtuple
-Pista = namedtuple("Pista", "x y dica")
-STYLE.update(width=850, height="650px")
-FIOFRONT = "https://i.imgur.com/wdwjGCt.jpg"
-AJUDA = "https://i.imgur.com/510Q3z4.png"
-FREDERICK = "https://i.imgur.com/4EtsjiX.jpg" #"https://i.imgur.com/377duSy.jpg"
-FOCO = "https://i.imgur.com/6e096Va.png"
-TABELA = "https://imgur.com/sotDlmO.png"
+STYLE.update(width=850, height="650px") # Atualiza o tamanho da tela
+
 
 class FioCruz:
     """ Cenário da FioCruz. """
     def __init__(self):
-        self.fc = Cena(FIOFRONT, direita=self, meio=self, esquerda=self)
-        self.ajuda = Elemento(AJUDA, x=470, y=510, w=300, h=40, cena=self.fc)
-        self.ajuda.elt.style.opacity = 0
-        
-    def vai(self, *_):
-        Texto(self.fc, "Você chegou na FioCruz, procure alguem para te ajudar").vai()
-        self.ajuda.vai = self.ajudar
-        
-    def ajudar(self, *_):
-        Texto(self.fc, "Siga pela direita para entrar no lab").vai()
-        self.fc.direita = LembrarHerdeitariedade() #Texto(self.fc, "ok você vai chegar lá")
-        
-        
+        self.fiocruz = None
+
     def inicia(self):
-        self.fc.vai()
+        """O jogo inicia aqui. O cenário principal será apresentado """
+        self.fiocruz = None
+
+
+if __name__ == "__main__":
+    fc = FioCruz()
+    fc.inicia()
+#####################################################################################################
 
 class EntenderHerdeitariedade:
     """Meu caro e querido aprendiz Damon, as informações que você precisa sobre HEREDITARIEDADE
@@ -55,44 +42,6 @@ class LembrarHerdeitariedade:
     Imagens: cena lab, caneca, tubo de ensaio, relógio, quadro, microscópio
     Na verdade, adaptei para perguntar a pessoas que já estavam na cena
     """
-    def __init__(self):
-        self.codigo = [
-        Pista(100, 70, "A única parte que sei é algo como 4-4, deixe eu pensar mais"),
-        Pista(600, 150, "Ah, a fórmula do Dr. Frederick tem 5-1, uma das meninas sabe o resto"),
-        Pista(750, 140, "Bem, o que você procura tem a ver com 3-2, a Débora deve saber mais"),
-        Pista(270, 120, "A Shirley é uma fofoqueira, mas já que ela já falou, posso te contar sobre o 5-1"),
-        Pista(400, 450, "A Joana uma vez falou algo como 2-2, lembro que alguém anotou o resto em algum lugar"),
-        Pista(700, 110, "No bloco você lê: Na verdade, tudo está em uma tabela colada no quadro"),
-        Pista(700, 110, "Você pega a tabela colada no quadro, talvez você possa decifrar algo"),
-        ]
-        p = self.codigo[0]
-        self.dica = 0
-        self.fc = Cena(FREDERICK, direita=self, meio=self, esquerda=self)
-        self.ajuda = Elemento(FOCO, x=p.x, y=p.y, w=150, h=150, cena=self.fc,vai=self.pista)
-        self.tabela = Elemento(TABELA, x=720, y=130, w=60, h=60, cena=self.fc)
-        self.ajuda.elt.style.opacity = 0.01
-        self.inicia = self.vai
-        
-    def pista(self, *_):
-        p = self.codigo[self.dica]
-        self.dica += 1
-        Texto(self.fc, p.dica).vai()
-        self.ajuda.elt.style.left = p.x
-        self.ajuda.elt.style.top = p.y
-        if self.dica > 6 :
-            self.tabela.elt.style.left = 170
-            self.tabela.elt.style.top = 120
-            self.tabela.elt.style.width = 500
-            self.tabela.elt.style.height = "500px"
-        
-    def vai(self, *_):
-        self.fc.vai()
-        Texto(self.fc, "Você chegou ao lab do Dr.Frederick, procure pistas da fórmula").vai()
-
-def grace():
-    fc = FioCruz()
-    fc = LembrarHerdeitariedade()
-    fc.inicia()
 
 #Inicio Jogo 1
 class LembraGene:
@@ -102,7 +51,7 @@ class LembraGene:
     Imagens soltas na bancada: Fragmento,DNA, Proteína, Molécula, Gene, Átomos, Ordene.
     """
 class EntendeGene:
-    """Entender-gene: A cientista sai, rola um pedra, cai uma grade, ela está ´presa, surge um enigma, 
+    """Entender-gene: A cientista sai, rola um pedra, cai uma grade, ela está Â´presa, surge um enigma, 
     acerte a charada, ela precisa correr, aparecem palavras desorganizadas. 
     Imagens: Núcleo, Cromossomo +histonas,Proteína
     """
@@ -177,17 +126,31 @@ class AnalisarRNA:
     """
 
 class AvaliarRNA:
-
     """Caminha para outra sala do museu. Aparece um cientista. Impede sua passagem. 
     Fala com uma voz assombrosa.
     """
 
 class CriarRNA:
-
     """No laboratório. Abre o computador. Pesquisa como acabar com a tristeza do planeta
     No microscópio observa a célula. Pega o RNA. Acrescenta novos códons. Muda sua forma.
     Produz serotonina.
     """
 
-if __name__ == "__main__":
-    grace()
+FIOFRONT = "https://i.imgur.com/wdwjGCt.jpg"
+AJUDA = "https://i.imgur.com/510Q3z4.png"
+FREDERICK = "https://i.imgur.com/4EtsjiX.jpg" #"https://i.imgur.com/377duSy.jpg"
+FOCO = "https://i.imgur.com/6e096Va.png"
+TABELA = "https://imgur.com/sotDlmO.png"
+FIOCRUZ = "https://i.imgur.com/pJDyRCt.jpg"
+BIBLIOTECA = "https://i.imgur.com/e6Oc9xU.jpg"
+COFRE = "https://i.imgur.com/obMPIpB.png"
+PENDRIVE = "https://i.imgur.com/18dBApo.png"
+CANECA ="https://i.imgur.com/El0wysJ.png"
+TUBO_DE_ENSAIO = "https://www.prolab.com.br/wp-content/uploads/2014/10/1-Tubosdeensaio.jpg"
+RELOGIO = "https://i.imgur.com/mevUdpA.png"
+QUADRO = "https://i.imgur.com/b2TqQsA.png"
+microscopio = "https://i.imgur.com/q414omp.png"
+GAVETA = "https://i.imgur.com/85Cta7F.jpg"
+ESCRITORIO = "https://i.imgur.com/7zvQ6PZ.jpg"
+COFREABERTO = "https://i.imgur.com/mgL4Gp2.png"
+TECLACOFRE = "https://i.imgur.com/9NiaFGL.png"
