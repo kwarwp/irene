49c2e141cf796440adfa7e2f47e39eb0a99ca19b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 12:13:57 -0300

    Saved naomi/__log__.py @2019-11-11 15:13

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -353,3 +353,19 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Mon Nov 11 2019 13:13:54.827 GMt-0200 (Brasilia Summer Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 66
+    fc.inicia()
+AttributeError: 'LembrarHerdeitariedade' object has no attribute 'inicia'
+'''},
