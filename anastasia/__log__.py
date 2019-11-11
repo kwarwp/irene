08ad1a6d7947c50c997c6ea154fa50904e8e2bab
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 11:51:05 -0300

    Created anastasia/__log__.py @2019-11-11 14:51

---
+++ anastasia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Nov 11 2019 11:50:41.387 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 35
+  """Meu caro e querido aprendiz Damon, as informações que você precisa sobre HEREDITARIEDADE
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           ^
+SyntaxError: invalid syntax
+'''},
