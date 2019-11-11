8ff1cfb92e0dde60bc75ab61cb5c6e44e1b4f261
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 12:34:13 -0300

    Created meredith/__log__.py @2019-11-11 15:34

---
+++ meredith/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Nov 11 2019 12:34:09.24 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 66
+  def _ajuda3_3(self, _=0):
+  ^
+IndentationError: unexpected indent
+'''},
