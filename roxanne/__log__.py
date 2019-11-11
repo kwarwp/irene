3a4c8c4500c7c6618f763106ac94d9c3b0328d36
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 11:02:37 -0300

    Saved roxanne/__log__.py @2019-11-11 14:02

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -119,3 +119,10 @@ IndentationError: expected an indented block
     self.ajuda = Elemento(FOCO, x=30, y=350, cena=self.fiocruz,
 AttributeError: 'FioCruz' object has no attribute '_ajuda'
 '''},
+{'date': 'Mon Nov 11 2019 11:02:18.560 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 38
+  def __init__(self):
+  ^
+IndentationError: unexpected indent
+'''},
