dec7bcf9e4d41dd54ee9272b860096e316f8c9bb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:48:56 -0300

    Saved sara/main.py @2019-11-11 13:48

--- sara/main.py
+++ sara/main.py
@@ -11,8 +11,8 @@ class FioCruz:
     """ Cenário da FioCruz. """
     def __init__(self):
         self.fiocruz = Cena(FIOCRUZ)
-        self.ajuda = Elemento(FOCO, x=30, y=350, cena=self.fiocruz)
-    def _ajuda(self, _ 0):
+        self.ajuda = Elemento(FOCO, x=30, y=350, cena=self.fiocruz, vai=self._äjuda)
+    def _ajuda(self, _=0):
         """O personagem dá uma explicação de como encontrar o laboratório """
         Texto(self.fiocruz, "O laboratório? Siga pela esquerda").vai()
     def inicia(self):
