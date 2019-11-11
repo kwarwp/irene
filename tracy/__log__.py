e6ca89908926b23cd0d766a003ec4dde8dd3d72a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:46:20 -0300

    Created tracy/__log__.py @2019-11-11 13:46

---
+++ tracy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Nov 11 2019 10:46:16.670 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 15
+  self.ajuda=Elemento(FOCO, x= 30, y=30 cena=self.fiocruz)
+                                         ^
+SyntaxError: invalid syntax
+'''},
