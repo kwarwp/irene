b948676388f2569d6efd43446dfa5e06628e7d23
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:50:21 -0300

    Saved kathryn/main.py @2019-11-11 13:50

--- kathryn/main.py
+++ kathryn/main.py
@@ -1,28 +1,29 @@
 # irene.kathryn.main.py
 """Planejamento do jogo da Genética JAIE19
 """
-__author__ = "<Vania Sara>"
+__author__ = "<O seu nome aqui>"
 __version__ = "19.11.11"
 from _spy.vitollino.main import STYLE, Cena, Elemento, Texto
 STYLE.update(width=850, height="650px") # Atualiza o tamanho da tela
-FOCO = "http://i.imgur.com/6e096Va.png"
-FIOCRUZ = "http://i.imgur.com/pJDyRCt.jpg"
+FOCO = "https://i.imgur.com/6e096Va.png"
+FIOCRUZ = "https://i.imgur.com/pJDyRCt.jpg"
 
 
 class FioCruz:
     """ Cenário da FioCruz. """
     def __init__(self):
         self.fiocruz = Cena(FIOCRUZ)
-        self.ajuda = Elemento(FOCO, x= 30, y=300, cena=self.fiocruz)
-        
+        self.ajuda = Elemento(FOCO, x=30, y=350, cena=self.fiocruz, vai=self._ajuda)
+
     def _ajuda(self, _=0):
-        """O personagem dá uma explicação de como encontrar o lab"""
-        Texto(self.fiocruz, "O laboratório?Siga pela esquerda").vai()
-        
+        """O personagem dá uma explicação de como encrontrar o lab """
+        Texto(self.fiocruz, "O laboratório? Siga pela esquerda").vai()
+
     def inicia(self):
         """O jogo inicia aqui. O cenário principal será apresentado """
         self.fiocruz.vai()
         Texto(self.fiocruz, "Temos que achar o lab. Vamos perguntar a alguém").vai()
+
 
 if __name__ == "__main__":
     fc = FioCruz()
