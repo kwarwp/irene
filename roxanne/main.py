c475b130a47e5142efbf76c37b8c28a72828a73c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:48:44 -0300

    Saved roxanne/main.py @2019-11-11 13:48

--- roxanne/main.py
+++ roxanne/main.py
@@ -8,16 +8,21 @@ STYLE.update(width=850, height="650px") # Atualiza o tamanho da tela
 FOCO = "https://i.imgur.com/6e096Va.png"
 FIOCRUZ = "https://i.imgur.com/pJDyRCt.jpg"
 
+
 class FioCruz:
     """ Cenário da FioCruz. """
     def __init__(self):
         self.fiocruz = Cena(FIOCRUZ)
-        self.ajuda = Elemento (FOCO, x=30, y=350, cena=self.fiocruz)
+        self.ajuda = Elemento(FOCO, x=30, y=350, cena=self.fiocruz, vai=self._ajuda)
+
+    def _ajuda(self, _=0):
+        """O personagem dá uma explicação de como encrontrar o lab """
+        Texto(self.fiocruz, "O laboratório? Siga pela esquerda").vai()
 
     def inicia(self):
         """O jogo inicia aqui. O cenário principal será apresentado """
         self.fiocruz.vai()
-        Texto(self.fiocruz, "Temos que achar o laboratório. Vamos perguntar a alguém?").vai()
+        Texto(self.fiocruz, "Temos que achar o lab. Vamos perguntar a alguém").vai()
 
 
 if __name__ == "__main__":
