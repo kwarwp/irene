8e15393d1df5500949533bfb719fe2591b21c81a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:43:09 -0300

    Created kathryn/__log__.py @2019-11-11 13:43

---
+++ kathryn/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Nov 11 2019 10:43:02.248 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 18
+  def inicia(self, =0):
+                    ^
+SyntaxError: invalid syntax
+'''},
