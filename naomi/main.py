8adfea02306876dbecefbcb7296c6181da3377c2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 11:12:13 -0300

    Saved naomi/main.py @2019-11-11 14:12

--- naomi/main.py
+++ naomi/main.py
@@ -10,20 +10,20 @@ FIOCRUZ = "https://i.imgur.com/pJDyRCt.jpg"
 
 class FioCruz:
     """ Cenário da FioCruz. """
-  def __init__(self):
-     self.fiocruz = Cena(FIOCRUZ)
-     self.ajuda = Elemento(FOCO, x=30, y=350, cena=self.lab, style={"opacity":0},vai=self._ajuda)
-        
-        
-  def ajuda(self, _=0):
-        """ O personagem dá uma explicação de como encontrar o la"""
+    def __init__(self):
+    self.fiocruz = Cena(FIOCRUZ)
+    self.fiocruz = Elemento(FOCO, x=30, y=350, cena=self.lab, style={"opacity":0},vai=self._ajuda)
+    
+    def ajuda(self, _=0):
+    """ O personagem dá uma explicação de como encontrar o la"""
         Texto=(self.fiocruz, "O laboratório? Pela esquerda.").vai()
-
-  def inicia(self):
+        
+        def inicia(self):
         """O jogo inicia aqui. O cenário principal será apresentado """
         self.fiocruz.vai()
         Texto(self.fiocruz, "Temos que achar o lab. Vamos perguntar a alguém?").vai()
-if __name__ == "__main__":
+        
+        if __name__ == "__main__":
     fc = FioCruz()
     fc.inicia()
 
