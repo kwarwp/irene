3d0c0b910ff0e1e211f88306cdb6637ba2044e0e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 11:45:49 -0300

    Saved alexa/main.py @2019-11-11 14:45

--- alexa/main.py
+++ alexa/main.py
@@ -10,6 +10,7 @@ STYLE.update(width=850, height="650px") # Atualiza o tamanho da tela
 FOCO = "https://i.imgur.com/6e096Va.png"
 FIOCRUZ = "https://i.imgur.com/pJDyRCt.jpg"
 FREDERICK = "https://i.imgur.com/4EtsjiX.jpg"
+CANECA ="https://i.imgur.com/El0wysJ.png"
 
 class FioCruz:
     """ Cenário da FioCruz. """
@@ -46,6 +47,8 @@ class Laboratorio:
             
         self.dica_persnagem_2 = Elemento(FOCO, x=260, y=140, cena=self.lab, 
             style={"opacity": 0.1})
+            
+        self.dica_caneca = Elemento(CANECA, x=500, y=500, cena=self.lab)
 
     def ajuda_4_4(self, _=0):
         """O personagem dará uma dica de como encontrar a fórmula."""
@@ -53,7 +56,7 @@ class Laboratorio:
         Texto(self.lab, "Eu acredito que o cientista falou que começa com 4-4, " + 
         "mas preciso lembrar do próximo...").vai()
         
-    def ajuda_5_1(self, _=0):
+    def ajuda_5_1_1(self, _=0):
         """O personagem dará uma dica de como encontrar a fórmula."""
         self.dica_persnagem_2.vai = self.ajuda_3_2
         Texto(self.lab, "Ah, lembrei, depois vem a 5-1. " +
@@ -61,7 +64,12 @@ class Laboratorio:
 
     def ajuda_3_2(self, _=0):
         """O personagem dará uma dica de como encontrar a fórmula."""
-        Texto(self.lab, "Só sei que depois da 5-1, vem o 3-2!").vai()
+        self.dica_caneca.vai = self.ajuda_5_1_2
+        Texto(self.lab, "Só sei que depois da 5-1, vem o 3-2! Ele gostava muito de café!").vai()
+        
+    def ajuda_5_1_2(self, _=0):
+        """O personagem dará uma dica de como encontrar a fórmula."""
+        Texto(self.lab, "Você encontrou um papelzinho com o código 5-1!").vai()
 
     def inicia(self):
         """Esse é o lab do cientista."""
