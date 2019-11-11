0faa3ef10d4d1d7e586fe6af67d25c123352dc99
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:47:49 -0300

    Saved parisa/main.py @2019-11-11 13:47

--- parisa/main.py
+++ parisa/main.py
@@ -15,11 +15,11 @@ class FioCruz:
     """ Cenário da FioCruz. """
     def __init__(self):
         self.fiocruz = Cena(FIOCRUZ)
-        self.ajuda = Elemento(FOCO, x=30, y=350, cena = self.fiocruz)
+        self.ajuda = Elemento(FOCO, x=30, y=350, cena = self.fiocruz, vai=self._ajuda)
 	
     def _ajuda(self, _=0):
         """ O personagem dá uma explicação de como encontrar o lab. """
-        Text(self.fiocruz, "O laboratório está no segundo andar da fio cruz. Sala 2005.")
+        Text(self.fiocruz, "O laboratório está no segundo andar da fio cruz. Sala 2005.").vai()
     
     def inicia(self):
         """O jogo inicia aqui. O cenário principal será apresentado """
