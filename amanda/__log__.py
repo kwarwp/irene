13abcbcb1817eeebbdb99e89095afd0a685fcbad
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 11:36:54 -0300

    Saved amanda/__log__.py @2019-11-11 14:36

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -96,3 +96,10 @@ NameError: name 'cena' is not defined
                                                                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Nov 11 2019 11:36:52.918 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 52
+  """O personagem dá uma explicação de como encontrarlaboratório """ self.denise.vai = self._ajuda3_2
+                                                                      ^
+SyntaxError: invalid syntax
+'''},
