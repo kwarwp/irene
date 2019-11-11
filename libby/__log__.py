1b84e2186f49d54dbe3ee40ae44aa2fb605a7da2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:52:11 -0300

    Created libby/__log__.py @2019-11-11 13:52

---
+++ libby/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Nov 11 2019 10:52:10.564 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 15
+  self.ajuda = Elemento(FOCO, x=30, y=350, cena=self.fiocruz,
+                                                                                       ^
+SyntaxError: invalid syntax
+'''},
