09735b0977dbc74c1226824128a05d311db0ebfb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:38:40 -0300

    Saved parisa/__log__.py @2019-11-11 13:38

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -35,3 +35,21 @@ NameError: name 'FIOCRUZ' is not defined
     self.ajuda = Element(FOCO, cena = self.fiocruz)
 NameError: name 'Element' is not defined
 '''},
+{'date': 'Mon Nov 11 2019 10:38:37.465 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 29
+    fc.inicia()
+  module <module> line 24
+    se
+NameError: name 'se' is not defined
+'''},
