1cf073d8d4411baf7a8d4746a6bd776c00200e68
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:43:36 -0300

    Saved parisa/main.py @2019-11-11 13:43

--- parisa/main.py
+++ parisa/main.py
@@ -15,7 +15,7 @@ class FioCruz:
     """ Cenário da FioCruz. """
     def __init__(self):
         self.fiocruz = Cena(FIOCRUZ)
-        self.ajuda = Elemento(FOCO, x=30, y=350, opacity=0.5, cena = self.fiocruz)
+        self.ajuda = Elemento(FOCO, x=30, y=350, cena = self.fiocruz)
 	
     def _ajuda(self, _=0):
         """ O personagem dá uma explicação de como encontrar o lab. """
