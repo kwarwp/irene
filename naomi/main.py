14e7d49a804c797265f80b6acec8a1c1dfe60ae4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:29:31 -0300

    Created naomi/main.py @2019-11-11 13:29

---
+++ naomi/main.py
@@ -0,0 +1,26 @@
+# irene.naomi.main.py
+"""Planejamento do jogo da Genética JAIE19
+"""
+__author__ = "<O seu nome aqui>"
+__version__ = "19.11.11"
+from _spy.vitollino.main import STYLE, Cena, Elemento, Texto
+STYLE.update(width=850, height="650px") # Atualiza o tamanho da tela
+FOCO = "https://i.imgur.com/6e096Va.png"
+FIOCRUZ = "https://i.imgur.com/pJDyRCt.jpg"
+
+class FioCruz:
+    """ Cenário da FioCruz. """
+    def __init__(self):
+        self.fiocruz = Cena(FIOCRUZ)
+
+    def inicia(self):
+        """O jogo inicia aqui. O cenário principal será apresentado """
+        self.fiocruz.vai()
+        
+        
+if__author__ = "Ivete"
+ft = FiioCruz()
+ft.inicia
+
+
+
