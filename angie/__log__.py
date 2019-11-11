85a578256f5fc1055d26eb516e553d67af701d47
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 11:46:12 -0300

    Saved angie/__log__.py @2019-11-11 14:46

--- angie/__log__.py
+++ angie/__log__.py
@@ -67,3 +67,23 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Nov 11 2019 11:46:36.151 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 77
+    fc = LembrarHerdeitariedade() #FioCruz()
+  module <module> line 53
+    self.caixametal = Elemento(CAIXAMETAL, x=300, y=380, cena=self.lab,
+  module _spy.vitollino.main line 518
+    self.img, self.title, self.dropper, self.alt = img, tit, drop, alt
+TypeError: can't set attributes of built-in/extension type 'object'
+'''},
