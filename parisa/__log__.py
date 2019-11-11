524ea993f84040790567ea9cfda01606747e2255
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 12:07:14 -0300

    Saved parisa/__log__.py @2019-11-11 15:07

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -85,3 +85,21 @@ SyntaxError: invalid syntax
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Mon Nov 11 2019 12:07:12.601 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 86
+    fc = FioCruz()
+  module <module> line 20
+    self.irProLab = Elemento(FOCO, x=800, y=600, cena=self.fiocruz, style={"opacity": 1}, vai=self._irProLab)
+AttributeError: 'FioCruz' object has no attribute '_irProLab'
+'''},
