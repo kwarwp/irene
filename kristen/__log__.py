cba483f4701534df8507ff7a634a04a8f1fc677e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:55:59 -0300

    Created kristen/__log__.py @2019-11-11 13:55

---
+++ kristen/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Nov 11 2019 10:55:56.305 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 21
+  def inicia(self):
+  ^
+IndentationError: unexpected indent
+'''},
