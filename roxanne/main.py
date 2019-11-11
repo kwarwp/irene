52a6de86e9f8feaceab736e59b91ecb15dbfeae3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 11:22:22 -0300

    Saved roxanne/main.py @2019-11-11 14:22

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,7 +1,7 @@
 # irene.roxanne.main.py
 """Planejamento do jogo da Genética JAIE19
 """
-__author__ = "<O seu nome aqui>"
+__author__ = "<Cristiane Jorge cristiane.bonfim@ifb.edu.br>"
 __version__ = "19.11.11"
 from _spy.vitollino.main import STYLE, Cena, Elemento, Texto
 STYLE.update(width=850, height="650px") # Atualiza o tamanho da tela
@@ -13,18 +13,17 @@ class FioCruz:
     """ Cenário da FioCruz. """
     def __init__(self):
         self.fiocruz = Cena(FIOCRUZ)
-        self.ajuda = Elemento(FOCO, x=0, y=350, cena=self.fiocruz,
-             style={"opacity": 0},vai=self._ajuda)
+        self.ajuda = Elemento(FOCO, x=30, y=350, cena=self.fiocruz,
+        style={"opacity":0},vai=self._ajuda)
 
     def _ajuda(self, _=0):
-        """O personagem dá uma explicação de como encrontrar o lab """
+        """O personagem dá uma explicação  de como encontrar o lab"""
         Texto(self.fiocruz, "O laboratório? Siga pela esquerda").vai()
 
     def inicia(self):
         """O jogo inicia aqui. O cenário principal será apresentado """
         self.fiocruz.vai()
-        Texto(self.fiocruz, "Temos que achar o lab. Vamos perguntar a alguém").vai()
-
+        Texto(self.fiocruz, "Temos que achar o laboratorio. Vamos perguntas a alguém").vai()
 
 class LembrarHerdeitariedade:
     """Após a morte do cientista Frederick, planejavam roubar sua fórmula em seu laboratório,
@@ -39,20 +38,32 @@ class LembrarHerdeitariedade:
     def __init__(self):
         self.lab = Cena(FREDERICK)
         self.ajuda = Elemento(FOCO, x=130, y=100, cena=self.lab,
-             style={"opacity": 1},vai=self._ajuda)
+        style={"opacity":0},vai=self._ajuda)
+        self.denise = Elemento(FOCO, x=230, y=100, cena=self.lab,
+        style={"opacity":1})
 
     def _ajuda(self, _=0):
-        """O personagem dá uma explicação de como encrontrar o lab """
-        Texto(self.lab, "O fórmula, tem a ver com 4-4? daqui a pouco lembro mais").vai()
+        """O personagem dá uma explicação  de como encontrar a fórmula"""
+        self.ajuda.vai = self.ajuda5_1
+        Texto(self.lab, "Lembrei tem 5-17 A Denise deve saber mais").vai()
+    
+    def _ajuda5_1(self, _=0):
+        """O personagem dá uma explicação  de como encontrar o lab"""
+        Texto(self.lab, "O laboratório? Siga pela esquerda").vai()
+               
+    def _ajuda3_2(self, _=0):
+        """O personagem dá uma nova explicação de como encontrar o lab"""
+        Texto(self.lab, "O laboratório? Siga pela esquerda").vai()
 
     def inicia(self):
         """O jogo inicia aqui. O laboratório será apresentado """
         self.lab.vai()
-        Texto(self.lab, "Temos que achar a fórmula. Vamos perguntar a alguém").vai()
-
+        Texto(self.lab, "Temos que achar a fórmula. Vamos perguntas a alguém").vai()
+    
 if __name__ == "__main__":
-    fc = LembrarHerdeitariedade() #FioCruz()
+    fc = LembrarHerdeitariedade() #Laboratório do Dr Frederick
     fc.inicia()
+    
 #####################################################################################################
 
 class EntenderHerdeitariedade:
@@ -64,6 +75,17 @@ class EntenderHerdeitariedade:
     
     Imagens: cena lab, cofre, pendrive, tabela
     """    
+
+class LembrarHerdeitariedade:
+    """Após a morte do cientista Frederick, planejavam roubar sua fórmula em seu laboratório,
+    seu fiel amigo e aprendiz Damon, precisa procurar a fórmula para protegê-la que está no laboratório,
+    porém seu amigo antes de morrer disse-lhe "botei a fórmula dentro deste cofre, cujo a senha é formada
+    por letras, que estão espalhadas em meu laboratório.". Existe uma tabela deixada pelo cientitsa para
+    decifrar quais letras formam a palavra-chave (4-4, 5-1, 3-2, 5-1, 2-2= GENES). 
+    
+    Imagens: cena lab, caneca, tubo de ensaio, relógio, quadro, microscópio
+    Na verdade, adaptei para perguntar a pessoas que já estavam na cena
+    """
 
 #Inicio Jogo 1
 class LembraGene:
