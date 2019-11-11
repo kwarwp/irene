633ebbf4699db57135e649980d36cfd02e088942
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:44:33 -0300

    Saved sara/main.py @2019-11-11 13:44

--- sara/main.py
+++ sara/main.py
@@ -12,7 +12,8 @@ class FioCruz:
     def __init__(self):
         self.fiocruz = Cena(FIOCRUZ)
         self.ajuda = Elemento(FOCO, x=30, y=350, cena=self.fiocruz)
-    def incia(self):"""O personagem dá uma explicação de como encontrar o laboratório """
+    def incia(self):
+        """O personagem dá uma explicação de como encontrar o laboratório """
         Texto(self.fiocruz, "O laboratório? Siga pela esquerda").vai()
     def inicia(self):
         """O jogo inicia aqui. O cenário principal será apresentado """
