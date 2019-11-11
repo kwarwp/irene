3b5d68386da35900ad5b442d3c60d029593da0d3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:35:26 -0300

    Saved sara/__log__.py @2019-11-11 13:35

--- sara/__log__.py
+++ sara/__log__.py
@@ -6,3 +6,10 @@
                                                                                                                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Nov 11 2019 09:35:11.564 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 18
+  Texto(self.fiocruz, "Procure setor de informações, precisamos encontrar o laboratório!".vai()
+                                                                                                                           ^
+SyntaxError: invalid syntax
+'''},
