f4924811a7e5e69eb7158d9f889da3c298cd1ee7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 11:02:50 -0300

    Created kellee/__log__.py @2019-11-11 14:02

---
+++ kellee/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Nov 11 2019 11:02:48.47 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 38
+  def __init__(self):
+  ^
+IndentationError: unexpected indent
+'''},
