3036d87b006d047be0e6537ea57b89644ea9fd5c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:40:35 -0300

    Saved tracy/main.py @2019-11-11 13:40

--- tracy/main.py
+++ tracy/main.py
@@ -17,7 +17,7 @@ class FioCruz:
     def inicia(self):
         """O jogo inicia aqui. O cenário principal será apresentado """
         self.fiocruz.vai()
-        Texto(self.fiocruz, "Temos que achar o lab. Vamos perguntar a alguém").vai()
+        Texto(self.fiocruz, "Temos que achar o laboratório. Vamos perguntar a alguém").vai()
 
 
 if __name__ == "__main__":
