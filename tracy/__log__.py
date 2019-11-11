00363f74d49969aba9fd56e3e905e30b0e0d2de6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:48:18 -0300

    Saved tracy/__log__.py @2019-11-11 13:48

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -6,3 +6,10 @@
                                          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Nov 11 2019 10:48:17.921 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  def ajud(self=0, _=0:
+                       ^
+SyntaxError: invalid syntax
+'''},
