39ec396a3faae5f4b6dd717849213c52404cfc6f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:26:50 -0300

    Saved kristen/main.py @2019-11-11 13:26

--- kristen/main.py
+++ kristen/main.py
@@ -5,15 +5,17 @@ __author__ = "Samuel Novaes Sena"
 __version__ = "19.11.11"
 from _spy.vitollino.main import STYLE, Cena, Elemento, Texto
 STYLE.update(width=850, height="650px") # Atualiza o tamanho da tela
+FOCO = "https://i.imgur.com/6e096Va.png"
+FIOCRUZ = "https://i.imgur.com/pJDyRCt.jpg"
 
 class FioCruz:
     """ Cenário da FioCruz. """
     def __init__(self):
-        self.fiocruz = None
+        self.fiocruz = Cena(FIOCRUZ)
 
     def inicia(self):
         """O jogo inicia aqui. O cenário principal será apresentado """
-        self.fiocruz = None
+        self.fiocruz.vai()
 
 if __name__ == "__main__":
     fc = FioCruz()
