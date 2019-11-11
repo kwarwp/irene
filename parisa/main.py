8454a56844008e05f94a59167b1b50766dc096ab
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:22:40 -0300

    Saved parisa/main.py @2019-11-11 13:22

--- parisa/main.py
+++ parisa/main.py
@@ -1,11 +1,15 @@
 # irene.roxanne.main.py
+
 """Planejamento do jogo da Genética JAIE19
 """
 __author__ = "<Oto Assunção - oto.braz@outlook.com>"
 __version__ = "19.11.11"
+
 from _spy.vitollino.main import STYLE, Cena, Elemento, Texto
 STYLE.update(width=850, height="650px") # Atualiza o tamanho da tela
 
+FOCO = "https://i.imgur.com/6e096Va.png"
+FIOCRUZ = "https://i.imgur.com/pJDyRCt.jpg"
 
 class FioCruz:
     """ Cenário da FioCruz. """
@@ -14,7 +18,7 @@ class FioCruz:
 
     def inicia(self):
         """O jogo inicia aqui. O cenário principal será apresentado """
-        self.fiocruz = None
+        self.fiocruz = FIOCRUZ
 
 
 if __name__ == "__main__":
