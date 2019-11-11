922a36a282aa0c35dd6651c6eec92606ab6aa471
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:37:36 -0300

    Created alexa/__log__.py @2019-11-11 13:37

---
+++ alexa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Nov 11 2019 10:37:34.473 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  self.ajuda = Elemento(FOCO, x=100, y=500 cena=self.fiocruz)
+                                            ^
+SyntaxError: invalid syntax
+'''},
