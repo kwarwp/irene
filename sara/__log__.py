059f9aae62bee6f90c8bd0d7bef3fadd4960fde1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 12:00:38 -0300

    Saved sara/__log__.py @2019-11-11 15:00

--- sara/__log__.py
+++ sara/__log__.py
@@ -98,3 +98,21 @@ IndentationError: unexpected indent
     self.denise = Elemento(Foco, x=230, y=100, cena=self.lab, style={"opacity": 1})
 NameError: name 'Foco' is not defined
 '''},
+{'date': 'Mon Nov 11 2019 11:00:23.364 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 62
+    fc.inicia()
+  module <module> line 57
+    self.fiocruz.vai()
+AttributeError: 'LembrarHereditariedade' object has no attribute 'fiocruz'
+'''},
