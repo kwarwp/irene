ccc8f42d83f06716fd854d4afb987b9cc5c94363
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:43:23 -0300

    Saved ruzwana/main.py @2019-11-11 13:43

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -14,7 +14,7 @@ class FioCruz:
         self.fiocruz = Cena(FIOCRUZ)
         self.ajuda = Elemento(FOCO,x=30,y=350, cena=self.fiocruz)
 
-    def ajuda(self):
+    def _ajuda(self,a=0):
         """O personagem dá uma explicação de como encontrar o laboratório """
         Texto(self.fiocruz,"O laboratório? Siga pela esquerda").vai()
         
