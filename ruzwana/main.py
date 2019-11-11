298405bb8cfb441e4539d75ebc6cfc127035f469
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:52:39 -0300

    Saved ruzwana/main.py @2019-11-11 13:52

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -12,7 +12,7 @@ class FioCruz:
     """ Cenário da FioCruz. """
     def __init__(self):
         self.fiocruz = Cena(FIOCRUZ)
-        self.ajuda = Elemento(FOCO,x=30,y=350, cena=self.fiocruz, vai=self._ajuda)
+        self.ajuda = Elemento(FOCO,x=30,y=350, cena=self.fiocruz, style={"opacity": 0}, vai=self._ajuda)
 
     def _ajuda(self, _=0):
         """O personagem dá uma explicação de como encontrar o laboratório """
@@ -22,7 +22,6 @@ class FioCruz:
         """O jogo inicia aqui. O cenário principal será apresentado """
         self.fiocruz.vai()
         Texto(self.fiocruz,"Temos que achar o laboratório. vamos perguntar a alguém").vai()
-
 
 if __name__ == "__main__":
     fc = FioCruz()
