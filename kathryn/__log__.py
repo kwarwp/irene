72ba3d46e71bd759e91b1b7aac19a3553b2754ca
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 11:08:28 -0300

    Saved kathryn/__log__.py @2019-11-11 14:08

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -20,3 +20,10 @@ SyntaxError: invalid syntax
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Nov 11 2019 11:08:22.239 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 38
+  def __init__(self):
+  ^
+IndentationError: unexpected indent
+'''},
