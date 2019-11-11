88f6ae9adcfa634d816d43495d7b86dbdab94b7f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:56:44 -0300

    Saved natalia/__log__.py @2019-11-11 13:56

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Nov 11 2019 10:56:58.321 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 18
+  style={"opacity":0},vai=self._ajuda)
+  ^
+IndentationError: unexpected indent
+'''},
