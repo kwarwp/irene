c876740e640d55055210ddb8f397b7974a9acfb8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:38:38 -0300

    Saved sarah/main.py @2019-11-11 13:38

--- sarah/main.py
+++ sarah/main.py
@@ -13,6 +13,7 @@ class FioCruz:
     """ Cenário da FioCruz. """
     def __init__(self):
         self.fiocruz = Cena(FIOCRUZ)
+        self.ajuda = Elemento(FOCO, x = 80, y = 340, cena = self.fiocruz)
 
     def inicia(self):
         """O jogo inicia aqui. O cenário principal será apresentado """
