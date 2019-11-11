0c5b8d265c2a7412674305e94c9a5d6a35f8ee18
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:42:12 -0300

    Saved kristen/main.py @2019-11-11 13:42

--- kristen/main.py
+++ kristen/main.py
@@ -9,14 +9,18 @@ FOCO = "https://i.imgur.com/6e096Va.png"
 FIOCRUZ = "https://i.imgur.com/pJDyRCt.jpg"
 
 class FioCruz:
-    """ Cenário da FioCruz. """
-    def __init__(self):
-        self.fiocruz = Cena(FIOCRUZ)
+	""" Cenário da FioCruz. """
+	def __init__(self):
+		self.fiocruz = Cena(FIOCRUZ)
+		self.ajuda = Elemento(FOCO, x=30, y=350, cena=self.fiocruz)
 
-    def inicia(self):
-        """O jogo inicia aqui. O cenário principal será apresentado """
-        self.fiocruz.vai()
-        Texto(self.fiocruz, "Temos que achar o lab. Vamos perguntar a alguém").vai()
+	def inicia(self):
+		"""O jogo inicia aqui. O cenário principal será apresentado """
+		self.fiocruz.vai()
+		Texto(self.fiocruz, "Temos que achar o lab. Vamos perguntar a alguém").vai()
+        
+	def ajuda(self):
+		Texto(self.fiocruz, "O laboratório? Siga pela esquerda").vai()
 
 if __name__ == "__main__":
     fc = FioCruz()
