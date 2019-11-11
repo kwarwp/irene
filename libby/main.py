cc244541bae7f2db1e95d3d32a91bade531ceb55
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:28:16 -0300

    Saved libby/main.py @2019-11-11 13:28

--- libby/main.py
+++ libby/main.py
@@ -4,17 +4,18 @@
 __author__ = "Matheus Dantas"
 __version__ = "19.11.11"
 from _spy.vitollino.main import STYLE, Cena, Elemento, Texto
-STYLE.update(width=850, height="650px") # Atualiza o tamanho da tela
-
+STYLE.update(width=960, height="540px") # Atualiza o tamanho da tela
+FOCO = "https://i.imgur.com/6e096Va.png"
+FIOCRUZ = "https://i.imgur.com/pJDyRCt.jpg"
 
 class FioCruz:
     """ Cenário da FioCruz. """
     def __init__(self):
-        self.fiocruz = None
+        self.fiocruz = Cena(FIOCRUZ)
 
     def inicia(self):
         """O jogo inicia aqui. O cenário principal será apresentado """
-        self.fiocruz = None
+        self.fiocruz.vai()
 
 
 if __name__ == "__main__":
