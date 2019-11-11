eab1560e6b88c9b7f3c648117e9f91622c360fc8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 11:04:19 -0300

    Saved parisa/main.py @2019-11-11 14:04

--- parisa/main.py
+++ parisa/main.py
@@ -10,12 +10,13 @@ STYLE.update(width=850, height="650px") # Atualiza o tamanho da tela
 
 FOCO = "https://i.imgur.com/6e096Va.png"
 FIOCRUZ = "https://i.imgur.com/pJDyRCt.jpg"
+FREDERICK = "https://i.imgur.com/4EtsjiX.jpg"
 
 class FioCruz:
     """ Cenário da FioCruz. """
     def __init__(self):
         self.fiocruz = Cena(FIOCRUZ)
-        self.ajuda = Elemento(FOCO, x=30, y=350, cena=self.fiocruz, style={"opacity": 0}, vai=self._ajuda)
+        self.ajuda = Elemento(FOCO, x=30, y=350, width=50, height=50, cena=self.fiocruz, style={"opacity": 0.3}, vai=self._ajuda)
 	
     def _ajuda(self, _=0):
         """ O personagem dá uma explicação de como encontrar o lab. """
@@ -24,10 +25,33 @@ class FioCruz:
     def inicia(self):
         """O jogo inicia aqui. O cenário principal será apresentado """
         self.fiocruz.vai()
-        # Texto(self.fiocruz, "Temos que achar o laboratório. Talvez alguem aqui saiba onde é.").vai()
+        Texto(self.fiocruz, "Temos que achar o laboratório. Talvez alguem aqui saiba onde é.").vai()
 
-
+class LembrarHerdeitariedade:
+    """Após a morte do cientista Frederick, planejavam roubar sua fórmula em seu laboratório,
+    seu fiel amigo e aprendiz Damon, precisa procurar a fórmula para protegê-la que está no laboratório,
+    porém seu amigo antes de morrer disse-lhe "botei a fórmula dentro deste cofre, cujo a senha é formada
+    por letras, que estão espalhadas em meu laboratório.". Existe uma tabela deixada pelo cientitsa para
+    decifrar quais letras formam a palavra-chave (4-4, 5-1, 3-2, 5-1, 2-2= GENES). 
+    
+    Imagens: cena lab, caneca, tubo de ensaio, relógio, quadro, microscópio
+    Na verdade, adaptei para perguntar a pessoas que já estavam na cena
+    """
+    
+    def __init__(self):
+        self.lab = Cena(FREDERICK)
+        self.ajuda = Elemento(FOCO, x=150, y=80, cena=self.lab, style={"opacity": 0.3}, vai=self._ajuda)
+	
+    def _ajuda(self, _=0):
+        """ O personagem dá uma explicação de como encontrar o lab. """
+        Texto(self.fiocruz, "Olha a fórmula está ").vai()
+    
+    def inicia(self):
+        """ Muada para o laboratório """
+        self.lab.vai()
+        Texto(self.lab, "Temos que achar a fórmula. Vamos perguntar à alguem").vai()
+        
 if __name__ == "__main__":
-    fc = FioCruz()
+    fc = LembrarHerdeitariedade()
     fc.inicia()
 #####################################################################################################
