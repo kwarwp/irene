fbde00790d93cf30a0918adec8e7a830ceb2e60b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:42:43 -0300

    Saved sara/__log__.py @2019-11-11 13:42

--- sara/__log__.py
+++ sara/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: invalid syntax
                                                                                                                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Nov 11 2019 09:42:28.272 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 16
+  Texto(self.fiocruz, "O laboratório? Siga pela esquerda").vai()
+  ^
+IndentationError: unexpected indent
+'''},
