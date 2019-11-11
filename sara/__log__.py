571f6b1151980c78726d7626a782e74531da4a4a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 11:58:35 -0300

    Saved sara/__log__.py @2019-11-11 14:58

--- sara/__log__.py
+++ sara/__log__.py
@@ -73,3 +73,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Nov 11 2019 10:58:20.754 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 37
+  def __init__(self):
+  ^
+IndentationError: unexpected indent
+'''},
