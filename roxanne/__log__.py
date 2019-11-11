d7194a00c9161b0c11dfb4e716a0a8b7ddf1fdfc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:38:41 -0300

    Created roxanne/__log__.py @2019-11-11 13:38

---
+++ roxanne/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Nov 11 2019 10:38:37.759 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 15
+  self.ajuda = Elemento (FOCO, x=80, y=500, cena=self,fiocruz)
+                                                             ^
+SyntaxError: non-keyword arg after keyword arg
+'''},
