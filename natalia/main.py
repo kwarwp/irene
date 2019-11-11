209f18f9bb8414d2302737f5b667d30a419ea4c4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:44:15 -0300

    Saved natalia/main.py @2019-11-11 13:44

--- natalia/main.py
+++ natalia/main.py
@@ -1,1 +1,27 @@
 # irene.natalia.main.py
+"""Planejamento do jogo da Genética JAIE19
+"""
+__author__ = "<César Bastos>"
+__version__ = "19.11.11"
+from _spy.vitollino.main import STYLE, Cena, Elemento, Texto
+STYLE.update(width=850, height="650px") # Atualiza o tamanho da tela
+FOCO = "https://i.imgur.com/6e096Va.png"
+TABELA = "https://imgur.com/sotDlmO.png"
+FIOCRUZ = "https://i.imgur.com/pJDyRCt.jpg"
+BIBLIOTECA = "https://i.imgur.com/e6Oc9xU.jpg"
+
+class FioCruz:
+    """ Cenário da FioCruz. """
+    def __init__(self):
+        self.fiocruz = Cena(FIOCRUZ)
+        self.ajuda = Elemento (FOCO, x=30, y=350, cena=self.fiocruz)
+       
+        def inicia(self):
+        """O jogo inicia aqui. O cenário principal será apresentado """
+        self.fiocruz.vai()
+        Texto(self.fiocruz, "Temos que achar o lab. Vamos perguntar a alguém").vai()
+
+
+if __name__ == "__main__":
+    fc = FioCruz()
+    fc.inicia()
