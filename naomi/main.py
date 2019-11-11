2f577b7522194fbe8df8b14fe824c496007ad5c1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 11:01:23 -0300

    Saved naomi/main.py @2019-11-11 14:01

--- naomi/main.py
+++ naomi/main.py
@@ -8,24 +8,24 @@ STYLE.update(width=850, height="650px") # Atualiza o tamanho da tela
 FOCO = "https://i.imgur.com/6e096Va.png"
 FIOCRUZ = "https://i.imgur.com/pJDyRCt.jpg"
 
-class FioCruz:
+  class FioCruz:
     """ Cenário da FioCruz. """
-	def __init__(self):
-	self.fiocruz = Cena(FIOCRUZ)
-	self.ajuda = Elemento(FOCO, x=30, y=350, cena=self.lab, style={"opacity":0},vai=self._ajuda)
+  def __init__(self):
+     self.fiocruz = Cena(FIOCRUZ)
+     self.ajuda = Elemento(FOCO, x=30, y=350, cena=self.lab, style={"opacity":0},vai=self._ajuda)
         
         
-	def ajuda(self, _=0):
+  def ajuda(self, _=0):
         """ O personagem dá uma explicação de como encontrar o la"""
         Texto=(self.fiocruz, "O laboratório? Pela esquerda.").vai()
 
-	def inicia(self):
+  def inicia(self):
         """O jogo inicia aqui. O cenário principal será apresentado """
         self.fiocruz.vai()
         Texto(self.fiocruz, "Temos que achar o lab. Vamos perguntar a alguém?").vai()
-	if __name__ == "__main__":
-	fc = FioCruz()
-	fc.inicia()
+if __name__ == "__main__":
+    fc = FioCruz()
+    fc.inicia()
 
 
 
