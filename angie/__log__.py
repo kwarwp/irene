4429bcc03575e5439fb5aa68b13d31c29fe85947
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 12:01:34 -0300

    Saved angie/__log__.py @2019-11-11 15:01

--- angie/__log__.py
+++ angie/__log__.py
@@ -147,3 +147,23 @@ TypeError: can't set attributes of built-in/extension type 'object'
     self.img, self.title, self.dropper, self.alt = img, tit, drop, alt
 TypeError: can't set attributes of built-in/extension type 'object'
 '''},
+{'date': 'Mon Nov 11 2019 12:01:58.139 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 73
+    fc = LembrarHerdeitariedade() #FioCruz()
+  module <module> line 48
+    self.caixametal = Elemento(CAIXAMETAL, x=300, y=280, cena=self.lab,
+  module _spy.vitollino.main line 518
+    self.img, self.title, self.dropper, self.alt = img, tit, drop, alt
+TypeError: can't set attributes of built-in/extension type 'object'
+'''},
