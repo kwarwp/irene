53b3e16cbe5f8391622159ddc86e7c4b01a1ab02
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:30:37 -0300

    Saved roxanne/main.py @2019-11-11 13:30

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,21 +1,20 @@
 # irene.roxanne.main.py
 """Planejamento do jogo da Genética JAIE19
 """
-__author__ = "<O seu nome aqui>"
+__author__ = "<Vania Sara>"
 __version__ = "19.11.11"
 from _spy.vitollino.main import STYLE, Cena, Elemento, Texto
 STYLE.update(width=850, height="650px") # Atualiza o tamanho da tela
-FOCO = "https://i.imgur.com/6e096VA.png"
-FIOCRUZ = "https://i.imgur.com/pJDyRCt.jpg"
+
 
 class FioCruz:
     """ Cenário da FioCruz. """
     def __init__(self):
-        self.fiocruz = Cena(FIOCRUZ)
+        self.fiocruz = None
 
     def inicia(self):
         """O jogo inicia aqui. O cenário principal será apresentado """
-        self.fiocruz.vai()
+        self.fiocruz = None
 
 
 if __name__ == "__main__":
