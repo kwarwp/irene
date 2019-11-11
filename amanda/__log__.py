ccfbe372305c6fea15331f237f06c63aa8095a9c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:30:19 -0300

    Saved amanda/__log__.py @2019-11-11 13:30

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -35,3 +35,21 @@ NameError: name 'CENA' is not defined
     self.fiocruz = CENA(FIOCRUZ)
 NameError: name 'CENA' is not defined
 '''},
+{'date': 'Mon Nov 11 2019 10:30:19.88 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 21
+    fc = FioCruz()
+  module <module> line 13
+    self.fiocruz = cena(FIOCRUZ)
+NameError: name 'cena' is not defined
+'''},
