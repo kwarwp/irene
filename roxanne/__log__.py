d772cec4738986f4c743d1f80689f9b2fa0d021c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 11:10:27 -0300

    Saved roxanne/__log__.py @2019-11-11 14:10

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -161,3 +161,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Nov 11 2019 11:10:09.25 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 38
+  def __init__(self):
+  ^
+IndentationError: unexpected indent
+'''},
