7c2178cedd7b3a0aa043a753cfe76cf95d79b6d1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:58:10 -0300

    Saved naomi/__log__.py @2019-11-11 13:58

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -172,3 +172,21 @@ AttributeError: 'FioCruz' object has no attribute '_ajuda'
     	self.ajuda = Elemento(FOCO, x=30, y=350, cena=self.fiocruz, style={"opacity":0},vai=self._ajuda)
 AttributeError: 'FioCruz' object has no attribute '_ajuda'
 '''},
+{'date': 'Mon Nov 11 2019 10:58:09.673 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 27
+    	fc = FioCruz()
+  module <module> line 15
+    	self.ajuda = Elemento(FOCO, x=30, y=350, cena=self.lab, style={"opacity":0},vai=self._ajuda)
+AttributeError: 'FioCruz' object has no attribute 'lab'
+'''},
