fe15b889a425abc200b73ad58bdb91816bfd0fc8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 11:37:51 -0300

    Saved roxanne/__log__.py @2019-11-11 14:37

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -188,3 +188,23 @@ IndentationError: unexpected indent
     self.img, self.title, self.dropper, self.alt = img, tit, drop, alt
 TypeError: can't set attributes of built-in/extension type 'object'
 '''},
+{'date': 'Mon Nov 11 2019 11:37:30.336 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 67
+    fc = LembrarHerdeitariedade() #Laboratório do Dr Frederick
+  module <module> line 44
+    self.caneca = Elemento(CANECA, x=740, y=480, cena=self.lab,
+  module _spy.vitollino.main line 518
+    self.img, self.title, self.dropper, self.alt = img, tit, drop, alt
+TypeError: can't set attributes of built-in/extension type 'object'
+'''},
