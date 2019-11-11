8738a8ff1b00dc1d8ee93637623efc1a36a9a986
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:22:58 -0300

    Saved sarah/main.py @2019-11-11 13:22

--- sarah/main.py
+++ sarah/main.py
@@ -1,10 +1,12 @@
 # irene.sarah.main.py
 """Planejamento do jogo da Genética JAIE19
 """
-__author__ = "<O seu nome aqui>"
+__author__ = "Amaury Antônio de Castro Junior"
 __version__ = "19.11.11"
 from _spy.vitollino.main import STYLE, Cena, Elemento, Texto
 STYLE.update(width=850, height="650px") # Atualiza o tamanho da tela
+FOCO = "https://i.imgur.com/6e096Va.png"
+FIOCRUZ = "https://i.imgur.com/pJDyRCt.jpg"
 
 
 class FioCruz:
