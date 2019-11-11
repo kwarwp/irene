aa06e6e636a2d87d6cd65e659ae76cb78c527382
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:33:37 -0300

    Saved roxanne/main.py @2019-11-11 13:33

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,25 +1,27 @@
 # irene.roxanne.main.py
 """Planejamento do jogo da Genética JAIE19
 """
-__author__ = "<Vania Sara>"
+__author__ = "<Cristiane Jorge cristiane.bonfim@ifb.edu.br>"
 __version__ = "19.11.11"
 from _spy.vitollino.main import STYLE, Cena, Elemento, Texto
 STYLE.update(width=850, height="650px") # Atualiza o tamanho da tela
-
+FOCO = "https://i.imgur.com/6e096Va.png"
+FIOCRUZ = "https://i.imgur.com/pJDyRCt.jpg"
 
 class FioCruz:
     """ Cenário da FioCruz. """
     def __init__(self):
-        self.fiocruz = None
+        self.fiocruz = Cena(FIOCRUZ)
 
     def inicia(self):
         """O jogo inicia aqui. O cenário principal será apresentado """
-        self.fiocruz = None
-
+        self.fiocruz.vai()
+        Texto(self.fiocruz, "Temos que achar o laboratorio. Vamos perguntas a alguém").vai()
 
 if __name__ == "__main__":
     fc = FioCruz()
     fc.inicia()
+    
 #####################################################################################################
 
 class EntenderHerdeitariedade:
