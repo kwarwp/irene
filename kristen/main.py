6ca808927c167362cf4e5dabd0827cc1c1b70eed
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 11:05:32 -0300

    Saved kristen/main.py @2019-11-11 14:05

--- kristen/main.py
+++ kristen/main.py
@@ -1,7 +1,7 @@
 # irene.kristen.main.py
 """Planejamento do jogo da Genética JAIE19
 """
-__author__ = "<Cristiane Jorge cristiane.bonfim@ifb.edu.br>"
+__author__ = "Samuel Novaes"
 __version__ = "19.11.11"
 from _spy.vitollino.main import STYLE, Cena, Elemento, Texto
 STYLE.update(width=850, height="650px") # Atualiza o tamanho da tela
