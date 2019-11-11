4ff3db113dd3261b7272c1859ea81d09033ba2a1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:58:35 -0300

    Saved libby/__log__.py @2019-11-11 13:58

--- libby/__log__.py
+++ libby/__log__.py
@@ -6,3 +6,10 @@
                                                                                        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Nov 11 2019 10:58:35.898 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 19
+  """O personagem dá uma explicação  de como encontrar o lab """
+  ^
+IndentationError: expected an indented block
+'''},
