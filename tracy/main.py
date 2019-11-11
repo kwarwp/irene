1558a36251709e77f59e070cb58388cee63dd0a0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:48:17 -0300

    Saved tracy/main.py @2019-11-11 13:48

--- tracy/main.py
+++ tracy/main.py
@@ -12,9 +12,9 @@ class FioCruz:
     """ Cenário da FioCruz. """
     def __init__(self):
         self.fiocruz = Cena(FIOCRUZ)
-        self.ajuda=Elemento(FOCO, x= 30, y=30 cena=self.fiocruz)
+        self.ajuda=Elemento(FOCO, x= 30, y=350, cena=self.fiocruz, vai=self.ajuda)
 
-    def ajud(self):
+    def ajud(self=0, _=0:
         """O personagem da uma explicação de como encontrar o laboratório """
         self.fiocruz.vai()
         Texto(self.fiocruz, "O laboratório? Siga pela esquerda").vai()
