4e70b29feb34f5008adb83f08d1e1d75ab82380b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 12:04:58 -0300

    Saved parisa/main.py @2019-11-11 15:04

--- parisa/main.py
+++ parisa/main.py
@@ -48,7 +48,7 @@ class LembrarHerdeitariedade:
         self.lab = Cena(FREDERICK)
         self.ajuda = Elemento(FOCO, x=120, y=80, cena=self.lab, style={"opacity": 0}, vai=self._ajuda)
         self.denise = Elemento(FOCO, x=280, y=150, cena=self.lab, style={"opacity": 0})
-        self.raquel = Elemento(FOCO, x=630, y=170, cena=self.lab, style={"opacity": 1})
+        self.raquel = Elemento(FOCO, x=630, y=180, cena=self.lab, style={"opacity": 1})
         self.amanda = Elemento(FOCO, x=780, y=150, cena=self.lab, style={"opacity": 1})
 
     def _ajuda(self, _=0):
@@ -70,11 +70,11 @@ class LembrarHerdeitariedade:
     def _ajuda3_2(self, _=0):
         """ A Denise dá uma explicação de como encontrar o lab. """
         self.amanda.vai = self._ajuda2_2
-        self.teste.vai = self._teste
+        self.raquel.vai = self._raquel
         Texto(self.lab, "O código que você precisa é o 3-2. Talvez a Amanda, que está do outro lado, saiba mais...").vai()
         
     def _raquel(self, _=0):
-        """ A Luisa diz que não é a Amanda. """
+        """ A Raquel diz que não é a Amanda. """
         Texto(self.lab, "Eu não sou a Amanda. É a moça do meu lado.").vai()
         
     def _ajuda2_2(self, _=0):
