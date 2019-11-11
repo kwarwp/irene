8e494c00a973fbbacb57780c915380452622a13d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:57:39 -0300

    Saved kristen/main.py @2019-11-11 13:57

--- kristen/main.py
+++ kristen/main.py
@@ -12,7 +12,7 @@ class FioCruz:
 	""" Cenário da FioCruz. """
 	def __init__(self):
 		self.fiocruz = Cena(FIOCRUZ)
-		self.ajuda = Elemento(FOCO, x=30, y=350, cena=self.fiocruz, style={"opacity":0},vai=self._ajuda)
+		self.ajuda = Elemento(FOCO, x=30, y=350, width=30, cena=self.fiocruz, style={"opacity": 0.5},vai=self._ajuda)
 
 	def _ajuda(self, _=0):
 		"""O personagem dá uma explicação  de como encontrar o lab"""
